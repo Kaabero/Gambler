7b595dda0f2603a05e058b3605312c28cff70104
Katri <katri.laamala@outlook.com>
2024-08-13 10:12:29 +0300

    render asetuksia

--- install_step.sh
+++ install_step.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+
+set -e
+
 echo "Install script"
 
-cd ./backend && npm install
-cd ./frontend && npm install
+
+cd ./backend 
+npm install
+
+
+cd ../frontend 
+npm install
--- start_step.sh
+++ start_step.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+
+set -e
+
 echo "Start script"
 
 
-cd ./frontend && rm -rf dist && npm run build && cp -r dist ../backend
-cd ./backend && npm run start
+cd ./frontend
+
+
+if [ -d dist ]; then
+  rm -rf dist
+fi
+
+
+npm run build
+
+
+cp -r dist ../backend
+
+
+cd ../backend
+
+
+npm run start
